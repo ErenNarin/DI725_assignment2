1eb46795ccc42f86b1545030a4ef3e2adbddd1af
eren <erennarin.92@gmail.com>
2025-04-13 21:57:31 +0300

    label histogram plot is added

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,14 +1,10 @@
-import albumentations as A
-import numpy as np
-
-
 def filter_invalid_bboxes(dataset):
     for idx in range(len(dataset)):
         sample = dataset[idx]
         valid_bboxes = []
         valid_labels = []
         bboxes = sample[1]["boxes"]
-        labels = sample[1]["labels"]
+        labels = sample[1]["class_labels"]
         for i, bbox in enumerate(bboxes):
             x_min, y_min, x_max, y_max = bbox[:4]
             if x_min < x_max and y_min < y_max:
@@ -20,83 +16,6 @@ def filter_invalid_bboxes(dataset):
                 )
 
         dataset[idx][1]["boxes"] = valid_bboxes
-        dataset[idx][1]["labels"] = valid_labels
+        dataset[idx][1]["class_labels"] = valid_labels
 
     return dataset
-
-
-def formatted_anns(image_id, category, area, bbox):
-    annotations = []
-    for i in range(0, len(category)):
-        new_ann = {
-            "image_id": image_id,
-            "category_id": category[i],
-            "isCrowd": 0,
-            "area": area[i],
-            "bbox": list(bbox[i]),
-        }
-        annotations.append(new_ann)
-
-    return annotations
-
-
-def convert_voc_to_coco(bbox):
-    xmin, ymin, xmax, ymax = bbox
-    width = xmax - xmin
-    height = ymax - ymin
-    return [xmin, ymin, width, height]
-
-
-def transform_aug_ann(examples, transform, image_processor):
-    image_ids = examples["image_id"]
-    images, bboxes, area, categories = [], [], [], []
-    for image, objects in zip(examples["image"], examples["objects"]):
-        image = np.array(image.convert("RGB"))[:, :, ::-1]
-        out = transform(image=image, bboxes=objects["bbox"], category=objects["category"])
-
-        area.append(objects["area"])
-        images.append(out["image"])
-
-        # Convert to COCO format
-        converted_bboxes = [convert_voc_to_coco(bbox) for bbox in out["bboxes"]]
-        bboxes.append(converted_bboxes)
-
-        categories.append(out["category"])
-
-    targets = [
-        {"image_id": id_, "annotations": formatted_anns(id_, cat_, ar_, box_)}
-        for id_, cat_, ar_, box_ in zip(image_ids, categories, area, bboxes)
-    ]
-
-    return image_processor(images=images, annotations=targets, return_tensors="pt")
-
-
-def transform_train(examples):
-    train_transform = A.Compose(
-        [
-            A.LongestMaxSize(500),
-            A.PadIfNeeded(500, 500, border_mode=0, value=(0, 0, 0)),
-            A.HorizontalFlip(p=0.5),
-            A.RandomBrightnessContrast(p=0.5),
-            A.HueSaturationValue(p=0.5),
-            A.Rotate(limit=10, p=0.5),
-            A.RandomScale(scale_limit=0.2, p=0.5),
-            A.GaussianBlur(p=0.5),
-            A.GaussNoise(p=0.5),
-        ],
-        bbox_params=A.BboxParams(format="pascal_voc", label_fields=["category"]),
-    )
-
-    return transform_aug_ann(examples, transform=train_transform)
-
-
-def transform_val(examples):
-    val_transform = A.Compose(
-        [
-            A.LongestMaxSize(500),
-            A.PadIfNeeded(500, 500, border_mode=0, value=(0, 0, 0)),
-        ],
-        bbox_params=A.BboxParams(format="pascal_voc", label_fields=["category"]),
-    )
-
-    return transform_aug_ann(examples, transform=val_transform)
--- utils/visualize.py
+++ utils/visualize.py
@@ -1,14 +1,12 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-from PIL import Image, ImageDraw
 
 
 def visualize_labels(image, target, id2label=None):
     fig, ax = plt.subplots(figsize=(12, 9))
     ax.imshow(image)
 
-    for box, label in zip(target["boxes"], target["labels"]):
+    for box, label in zip(target["boxes"], target["class_labels"]):
         x0, y0, x1, y1 = box.tolist()
         rect = patches.Rectangle((x0, y0), x1 - x0, y1 - y0,
                                  linewidth=2, edgecolor="red", facecolor="none", linestyle="--")
@@ -35,7 +33,7 @@ def visualize_predictions(image, preds, target, id2label=None, threshold=0.8):
             ax.text(x0, y0 - 4, f"{name}: {score:.2f}", color="lime", fontsize=10,
                     bbox=dict(facecolor='black', alpha=0.5, pad=1))
 
-    for box, label in zip(target["boxes"], target["labels"]):
+    for box, label in zip(target["boxes"], target["class_labels"]):
         x0, y0, x1, y1 = box.tolist()
         rect = patches.Rectangle((x0, y0), x1 - x0, y1 - y0,
                                  linewidth=2, edgecolor="red", facecolor="none", linestyle="--")
@@ -48,97 +46,32 @@ def visualize_predictions(image, preds, target, id2label=None, threshold=0.8):
     plt.show()
 
 
-def draw_image_from_idx(dataset, idx, id2label):
-    sample = dataset[idx]
-    image = sample[0]
-    annotations = sample["objects"]
-    draw = ImageDraw.Draw(image)
-    width, height = sample["width"], sample["height"]
+def label_histogram(dataset, id2label):
+    label_examples = {}
+    for example in dataset:
+        for label in example[1]["class_labels"]:
+            if id2label[label] not in label_examples:
+                label_examples[id2label[label]] = 1
+            else:
+                label_examples[id2label[label]] += 1
 
-    print(annotations)
+    categories = list(label_examples.keys())
+    values = list(label_examples.values())
 
-    for i in range(len(annotations["bbox_id"])):
-        box = annotations["bbox"][i]
-        x1, y1, x2, y2 = tuple(box)
-        draw.rectangle((x1, y1, x2, y2), outline="red", width=3)
-        draw.text((x1, y1), id2label[annotations["category"][i]], fill="green")
+    fig, ax = plt.subplots(figsize=(12, 8))
 
-    return image
+    bars = ax.bar(categories, values, color="skyblue")
 
+    ax.set_xlabel("Categories", fontsize=14)
+    ax.set_ylabel("Number of Occurrences", fontsize=14)
+    ax.set_title("Number of Occurrences by Category", fontsize=16)
 
-def plot_augmented_images(dataset, indices, transform=None):
-    """
-    Plot images and their annotations with optional augmentation.
-    """
-    num_rows = len(indices) // 3
-    num_cols = 3
-    fig, axes = plt.subplots(num_rows, num_cols, figsize=(15, 10))
+    ax.set_xticklabels(categories, rotation=90, ha="right")
+    ax.grid(axis="y", linestyle="--", alpha=0.7)
 
-    for i, idx in enumerate(indices):
-        row = i // num_cols
-        col = i % num_cols
-
-        # Draw augmented image
-        image = draw_augmented_image_from_idx(dataset, idx, transform=transform)
-
-        # Display image on the corresponding subplot
-        axes[row, col].imshow(image)
-        axes[row, col].axis("off")
+    for bar in bars:
+        height = bar.get_height()
+        ax.text(bar.get_x() + bar.get_width() / 2.0, height, f"{height}", ha="center", va="bottom", fontsize=10)
 
     plt.tight_layout()
     plt.show()
-
-
-def draw_augmented_image_from_idx(dataset, idx, id2label, transform=None):
-    sample = dataset[idx]
-    image = sample["image"]
-    annotations = sample["objects"]
-
-    # Convert image to RGB and NumPy array
-    image = np.array(image.convert("RGB"))[:, :, ::-1]
-
-    if transform:
-        augmented = transform(image=image, bboxes=annotations["bbox"], category=annotations["category"])
-        image = augmented["image"]
-        annotations["bbox"] = augmented["bboxes"]
-        annotations["category"] = augmented["category"]
-
-    image = Image.fromarray(image[:, :, ::-1])  # Convert back to PIL Image
-    draw = ImageDraw.Draw(image)
-    width, height = sample["width"], sample["height"]
-
-    for i in range(len(annotations["bbox_id"])):
-        box = annotations["bbox"][i]
-        x1, y1, x2, y2 = tuple(box)
-
-        # Normalize coordinates if necessary
-        if max(box) <= 1.0:
-            x1, y1 = int(x1 * width), int(y1 * height)
-            x2, y2 = int(x2 * width), int(y2 * height)
-        else:
-            x1, y1 = int(x1), int(y1)
-            x2, y2 = int(x2), int(y2)
-
-        draw.rectangle((x1, y1, x2, y2), outline="red", width=3)
-        draw.text((x1, y1), id2label[annotations["category"][i]], fill="green")
-
-    return image
-
-
-def plot_results(image, results, threshold=0.6):
-    image = Image.fromarray(np.uint8(image))
-    draw = ImageDraw.Draw(image)
-    width, height = image.size
-
-    for result in results:
-        score = result["score"]
-        label = result["label"]
-        box = list(result["box"].values())
-
-        if score > threshold:
-            x1, y1, x2, y2 = tuple(box)
-            draw.rectangle((x1, y1, x2, y2), outline="red", width=3)
-            draw.text((x1 + 5, y1 - 10), label, fill="white")
-            draw.text((x1 + 5, y1 + 10), f"{score:.2f}", fill="green" if score > 0.7 else "red")
-
-    return image
